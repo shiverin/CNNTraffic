6784181deb0768fa15e2d308da223279eca7b751
leo8477 <Your GitHub Email>
2025-06-04 01:06:18 +0800

    update traffic.py

--- traffic.py
+++ traffic.py
@@ -21,7 +21,7 @@ def main():
 
     # Get image arrays and labels for all image files
     images, labels = load_data(sys.argv[1])
-
+    #images = images / 255.0
     # Split data into training and testing sets
     labels = tf.keras.utils.to_categorical(labels)
     x_train, x_test, y_train, y_test = train_test_split(
@@ -47,28 +47,48 @@ def main():
 def load_data(data_dir):
     """
     Load image data from directory `data_dir`.
-
-    Assume `data_dir` has one directory named after each category, numbered
-    0 through NUM_CATEGORIES - 1. Inside each category directory will be some
-    number of image files.
-
-    Return tuple `(images, labels)`. `images` should be a list of all
-    of the images in the data directory, where each image is formatted as a
-    numpy ndarray with dimensions IMG_WIDTH x IMG_HEIGHT x 3. `labels` should
-    be a list of integer labels, representing the categories for each of the
-    corresponding `images`.
     """
-    raise NotImplementedError
-
+    labels=[]
+    images=[]
+    for folder_name in os.listdir(data_dir):
+        folder_path=os.path.join(data_dir,folder_name)
+        if os.path.isdir(folder_path):
+            l=int(folder_name)
+            for file in os.listdir(folder_path):
+                image_path=os.path.join(folder_path,file)
+                img = cv2.imread(image_path)
+                resized_img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+                images.append(resized_img)
+                labels.append(l)
+    return (images,labels)
 
 def get_model():
     """
-    Returns a compiled convolutional neural network model. Assume that the
-    `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
-    The output layer should have `NUM_CATEGORIES` units, one for each category.
+    Returns a compiled convolutional neural network model.
     """
-    raise NotImplementedError
+    model = tf.keras.Sequential()
+    # First add an Input layer with the shape
+    model.add(tf.keras.layers.Input(shape=(IMG_WIDTH, IMG_HEIGHT, 3)))
+    # Then add Conv2D with correct args order
+    model.add(tf.keras.layers.Conv2D(
+        32,               # number of filters
+        (3, 3),           # kernel size
+        activation='relu' # activation function
+    ))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.5))
+    model.add(tf.keras.layers.Dense(NUM_CATEGORIES, activation='softmax'))
 
+    model.compile(
+        optimizer='adam',
+        loss='categorical_crossentropy',  # since your labels are one-hot encoded
+        metrics=['accuracy']
+    )
+    return model
 
 if __name__ == "__main__":
     main()
